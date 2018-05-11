b6c8683ea102c159a0d0828ae75fa08afeacf097
Sebastian Eibl <sebastian.eibl@fau.de>
2018-05-11 18:45:06 +0200

    added some documentation

--- Car.h
+++ Car.h
@@ -12,13 +12,21 @@ struct Car
     Car(const int pos, const int vel) : x(pos), v(vel) {}
 };
 
+///data container of all cars
+///
+///This container has to be sorted at all times. back() car wich traveled the farest. first() newest car.
 using TrafficLine = std::deque<Car>;
 
-
+///acclerate all cars by 1
 void accelerate(TrafficLine& cars);
+///decelerate cars if a collision would be imminent with current velocity
 void decelerate(TrafficLine& cars);
+///cars dawdle (reduce their speed without any reason) with a certain probability
 void dawdle(TrafficLine& cars, const double p = 0.7);
+///time integrate the position of all cars
 void move(TrafficLine& cars);
 
+///add a car at the beginning of the street with a certain probability if there is no car
 void addCar(TrafficLine& cars, const double p = 1.0);
+///remove all cars which moved to far
 void removeCars(TrafficLine& cars, const int streetLength);
